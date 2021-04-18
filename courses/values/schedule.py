aabb41c1154eac7bff2746f8c60d747d26523f9d
Askar <a.almabek@trafficwave.com>
2021-04-18 19:26:19 +0600

    Change schedule subtraction

--- courses/values/schedule.py
+++ courses/values/schedule.py
@@ -14,7 +14,9 @@ class DayPeriod:
     day: int
     time: TimePeriod
 
-    def __sub__(self, other: 'DayPeriod') -> Optional['DayPeriod']:
+    def __sub__(self, other: Optional['DayPeriod']) -> Optional['DayPeriod']:
+        if not other:
+            return self
         start = other.time.end
         end = self.time.end
         if start == end:
@@ -30,11 +32,9 @@ class Schedule:
         periods = []
         for period in self.periods:
             other_period = other.find_period_for_day(period.day)
-            if other_period is None:
-                periods.append(period)
-                continue
             periods.append(period - other_period)
         return Schedule(periods)
 
     def find_period_for_day(self, day: int) -> Optional[DayPeriod]:
-        return next((period for period in self.periods if period.day == day), None)
+        return next((period for period in self.periods if period.day == day),
+                    None)
