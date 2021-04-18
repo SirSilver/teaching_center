c9a5b0422e5417bf0414dd397bf8740222152d4f
Askar <a.almabek@trafficwave.com>
2021-04-18 19:25:26 +0600

    Add datetime attribute to Lesson entity

--- courses/entities/course.py
+++ courses/entities/course.py
@@ -1,7 +1,7 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from typing import List, Optional
 
-from ..values import DayPeriod, Schedule
+from ..values import DayPeriod, Schedule, TimePeriod
 
 
 class Student:
@@ -12,20 +12,30 @@ class Student:
 
 class Lesson:
 
-    def __init__(self, period: DayPeriod, course_id: int) -> None:
-        self.period = period
+    def __init__(self, starts_at: datetime, duration: int,
+                 course_id: int) -> None:
+        self._starts_at = starts_at
+        self._duration = duration
         self._course_id = course_id
+
+    @property
+    def period(self) -> DayPeriod:
+        start = self._starts_at.time()
+        end = (datetime.combine(self._starts_at.date(), start)
+               + timedelta(minutes=self._duration)).time()
+        time = TimePeriod(start, end)
+        return DayPeriod(self._starts_at.weekday(), time)
 
 
 class Course:
 
     def __init__(self, schedule: Schedule, number_of_lessons: int,
-                 lesson_duration: int, lessons: List[Lesson],
+                 lesson_duration: int, registered_lessons: List[Lesson],
                  course_id: Optional[int]) -> None:
         self._schedule = schedule
         self.number_of_lessons = number_of_lessons
         self._lesson_duration = lesson_duration
-        self._lessons = lessons
+        self._registered_lessons = registered_lessons
         self.id = course_id
 
     def get_available_hours(self) -> Schedule:
@@ -33,5 +43,5 @@ class Course:
         return self._schedule - lessons_schedule
 
     def _get_lessons_schedule(self) -> Schedule:
-        period = [lesson.period for lesson in self._lessons]
+        period = [lesson.period for lesson in self._registered_lessons]
         return Schedule(period)
--- courses/tests/entities/test_course.py
+++ courses/tests/entities/test_course.py
@@ -1,4 +1,4 @@
-from datetime import time
+from datetime import datetime, time
 
 import pytest
 
@@ -8,12 +8,12 @@ from ...values import DayPeriod, Schedule, TimePeriod
 
 @pytest.fixture(scope='class')
 def setup(request) -> None:
-    period_1 = DayPeriod(day=1, time=TimePeriod(time(9, 0, 0), time(18, 0, 0)))
-    period_2 = DayPeriod(day=2, time=TimePeriod(time(9, 0, 0), time(18, 0, 0)))
+    period_1 = DayPeriod(day=0, time=TimePeriod(time(9, 0, 0), time(18, 0, 0)))
+    period_2 = DayPeriod(day=1, time=TimePeriod(time(9, 0, 0), time(18, 0, 0)))
     periods = [period_1, period_2]
     schedule = Schedule(periods)
-    period = DayPeriod(1, TimePeriod(time(9, 0, 0), time(10, 0, 0)))
-    lessons = [Lesson(period, 1)]
+    lessons = [Lesson(datetime(2021, 4, 19, 9, 0, 0), 60, 1),
+               Lesson(datetime(2021, 4, 20, 9, 0, 0), 60, 1)]
     request.cls.course = Course(schedule, 5, 60, lessons, 1)
 
 
@@ -24,6 +24,6 @@ class TestCourse:
 
     def test_can_find_available_hours(self) -> None:
         result = self.course.get_available_hours()
-        print(result.periods)
         assert result.periods[0].time.start.hour == 10
+        assert result.periods[1].time.start.hour == 10
         assert len(result.periods) == 2
